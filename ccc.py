0646f10eb59ca92fefd0453500b355a016e511bc
Sellal Hossain <111631860+Sellal-Hossain@users.noreply.github.com>
2022-09-14 18:45:08 +0600

    Add files via upload

---
+++ ccc.py
@@ -0,0 +1,15 @@
+#Tools Created By Sellal Hossain
+#All Hacking tools
+#BD Cloning 
+user:'Sellal'
+pass:'hossain'
+def menu():
+    os.system('clear')
+    print logo
+    print ''
+    print '\033[0;93mAUTHOR   : Sellal Hossain'
+    print '\033[0;94mYOUTUBE : All Hacking Tools '
+    print '\033[0;96mGITHUB    : github.com/Sellal-Hossain'
+    print '\033[0;98mNumber    : 017********
+    print''
+    
